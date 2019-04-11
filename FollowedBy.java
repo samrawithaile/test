347ee5be638e5d0be6b588ae7b944be28bf37899
bereketmichael741 <bereketmichael741@gmail.com>
2019-04-10 21:22:48 -0500

    i added attributes on the followedBy class

--- FollowedBy.java
+++ FollowedBy.java
@@ -1,5 +1,10 @@
 package demo.instangram.instagram.domain;
 
+import java.util.List;
+
 public class FollowedBy extends Person {
+	private long id;
+	private User user;
+	private List<User> userFollowingList;
 
 }
